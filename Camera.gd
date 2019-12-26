3af617b40fb82f2554f5ac8487eba857cb5ffb03
Joe <joe.zlonicky@gmail.com>
2019-12-26 00:40:00 -0800

    Update tiles (ui in progress)

--- Camera.gd
+++ Camera.gd
@@ -2,6 +2,7 @@ extends Camera2D
 
 
 var zoom_modifier = 1.03 # Should probably be between 1.01 and 1.2
+var camera_move_speed = 350
 
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed():
@@ -13,4 +14,15 @@ func _input(event):
 			if zoom.x * zoom_modifier <= 4:
 				zoom.x *= zoom_modifier
 				zoom.y *= zoom_modifier
-			
+				
+func _process(delta):
+	var movement = Vector2()
+	if Input.is_action_pressed("ui_up"):
+		movement.y -= camera_move_speed
+	if Input.is_action_pressed("ui_right"):
+		movement.x += camera_move_speed
+	if Input.is_action_pressed("ui_down"):
+		movement.y += camera_move_speed
+	if Input.is_action_pressed("ui_left"):
+		movement.x -= camera_move_speed
+	position += movement.normalized() * camera_move_speed * delta
